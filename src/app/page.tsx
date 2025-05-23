b25427d480a1990ee98be1adee6bcb3be1e0fc6b
정용성 <jys9049@blocko.io>
2025-05-23 21:43:57 +0900

    fix: build test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@
 export default function Page() {
   console.log(process.env.NEXT_PUBLIC_TEST_KEY ?? "no client test key");
 
-  return <h1>안녕하세요! 메인 페이지입니다.</h1>;
+  return <h1>안녕하세요! 메인 페이지입니다!!!.</h1>;
 }
